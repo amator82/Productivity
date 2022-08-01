8b45032e552d09647b703c323d3602cdba0f1427
Ilya <ilyakorckishko@gmail.com>
2022-08-01 13:42:56 +0300

    Initial commit

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import * as flsFunctions from './modules/functions.js'
+
+flsFunctions.isWebp()
+// Подключение меню бургер
+// flsFunctions.menuInit()
+
+// import Swiper, { Navigation, Pagination } from 'swiper';
+
+// const swiper = new Swiper ()
---
+++ src/js/modules/functions.js
@@ -0,0 +1,28 @@
+export function isWebp() {
+    function testWebP(callback) {
+        let webP = new Image()
+        webP.onload = webP.onerror = function () {
+            callback(webP.height == 2)
+        }
+        webP.src =
+            'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA'
+    }
+
+    testWebP(function (support) {
+        let className = support === true ? 'webp' : 'no-webp'
+        document.documentElement.classList.add(className)
+    })
+}
+//!<Menu Burger>
+export function menuInit() {
+    const iconMenu = document.querySelector('.menu__icon')
+    const menuBody = document.querySelector('.menu__body')
+    if (iconMenu) {
+        iconMenu.addEventListener('click', function (e) {
+            document.body.classList.toggle('_lock')
+            iconMenu.classList.toggle('_active')
+            menuBody.classList.toggle('_active')
+        })
+    }
+}
+//!</Menu Burger>
