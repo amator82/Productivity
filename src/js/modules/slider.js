cd9839be18417db5d4feadffb74b99f2d1a02d3c
Ilya <ilyakorckishko@gmail.com>
2022-08-04 17:19:20 +0300

    add slider functions

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -6,14 +6,29 @@ new Swiper('.main__swiper', {
     },
     loop: true,
     autoHeight: true,
+    simulateTouch: true,
+    touchRatio: 1.5,
+    slideToClickedSlide: true,
+    autoplay: {
+        delay: 5000
+    },
+    keyboard: {
+        //? Включить/выключить
+        enabled: true,
+        //? Включить/выключить только когда слайдер в пределах вьювпорта
+        onlyInviewport: true,
+        //? Включить/выключить управление клавишами PageUp, PageDown
+        pageUpDown: true
+    },
     breakpoints: {
         320: {
             slidesPerView: 1.05,
-            spaceBetween: 10,
+            spaceBetween: 10
         },
         768: {
             slidesPerView: 1,
             spaceBetween: 0,
+            grabCursor: true
         }
-    },
+    }
 })
