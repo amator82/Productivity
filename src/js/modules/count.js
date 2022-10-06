97b53d98c80e55ec704b145c62ba4434c897fdf5
Ilya <ilyakorckishko@gmail.com>
2022-10-06 16:41:01 +0300

    update counter

--- src/js/modules/count.js
+++ src/js/modules/count.js
@@ -1,34 +1,33 @@
-const items = [...document.querySelectorAll('.number')]
+// const items = [...document.querySelectorAll('.number')]
+
+const percents = document.querySelector('.item-customers__percents')
+const number = document.querySelector('.item-users__number')
 let isResizeble = false
 
 window.addEventListener('scroll', () => {
     const scrollHeight = Math.floor(window.pageYOffset)
 
-    if (!isResizeble && scrollHeight > 1900) {
-        items.forEach((item) => {
-            updateCount(item)
-        })
+    if (!isResizeble && scrollHeight > 1350) {
+        updateCount(percents, 10, 0, '%')
+        updateCount(number, 1, 2000, '+')
 
         isResizeble = true
     }
 })
 
-const updateCount = (el) => {
-    console.log(el)
+const updateCount = (el, interval, initialValue, decor) => {
     const value = parseInt(el.dataset.value)
     const increment = Math.ceil(value / 1000)
-
-    let initialValue = 0
 
     const increaseValue = setInterval(() => {
         initialValue += increment
 
         if (initialValue > value) {
-            el.textContent = `${value}`
+            el.textContent = `${value + decor}`
             clearInterval(increaseValue)
             return
         }
 
-        el.textContent = `${initialValue}`
-    }, 1)
+        el.textContent = `${initialValue + decor}`
+    }, interval)
 }
