ca8e6de628adb100a95fc189649118c215b5e473
Ilya <ilyakorckishko@gmail.com>
2022-10-28 16:49:28 +0300

    move data to separate file

---
+++ src/js/files/data.js
@@ -0,0 +1,274 @@
+const data = {
+    mainBlockSwiperData: [
+        {
+            id: 1,
+            info: 'PLAN YOUR LIFE',
+            title: `Increase your <span>productivity</span>`,
+            text: `
+                    Brute laoreet efficiendi id his, ea illum nonumes luptatum pro. Usu atqui laudem an, insolens gubergren similique est cu. Et vel modus congue vituperata.
+                `,
+            btn: '../../img/main-block/play.svg',
+            backgroungImage: `../../img/main-block/background.png`,
+            decor: {
+                1: `../../img/main-block/decor/01.svg`,
+                2: `../../img/main-block/decor/02.svg`,
+                3: `../../img/main-block/decor/03.svg`,
+                4: `../../img/main-block/decor/04.svg`,
+                5: `../../img/main-block/decor/05.svg`,
+                6: `../../img/main-block/decor/06.svg`,
+                7: `../../img/main-block/decor/07.svg`,
+                8: `../../img/main-block/decor/08.svg`,
+                9: `../../img/main-block/decor/09.svg`,
+                10: `../../img/main-block/decor/10.svg`,
+                11: `../../img/main-block/decor/11.svg`,
+                12: `../../img/main-block/decor/12.svg`,
+                13: `../../img/main-block/decor/13.svg`
+            }
+        },
+        {
+            id: 2,
+            info: 'Choose yourself',
+            title: `Choose your <span>character</span>`,
+            text: `
+                    I-I-I-I'm from Hell
+                    Deep within the darkest place you'll ever be
+                    I'm from Hell
+                    Do the shit that demons do and make you bleed
+                    I'm from Hell
+                    Make you witness shit that you won't wanna see
+                    What the hell?
+                `,
+            btn: '../../img/main-block/play.svg',
+            backgroungImage: `../../img/main-block/background.png`,
+            decor: {
+                1: `../../img/main-block/decor/01.svg`,
+                2: `../../img/main-block/decor/02.svg`,
+                3: `../../img/main-block/decor/03.svg`,
+                4: `../../img/main-block/decor/04.svg`,
+                5: `../../img/main-block/decor/05.svg`,
+                6: `../../img/main-block/decor/06.svg`,
+                7: `../../img/main-block/decor/07.svg`,
+                8: `../../img/main-block/decor/08.svg`,
+                9: `../../img/main-block/decor/09.svg`,
+                10: `../../img/main-block/decor/10.svg`,
+                11: `../../img/main-block/decor/11.svg`,
+                12: `../../img/main-block/decor/12.svg`,
+                13: `../../img/main-block/decor/13.svg`
+            }
+        },
+        {
+            id: 3,
+            info: 'Just feel it',
+            title: `Increase your <span>productivity</span>`,
+            text: `
+                    Brute laoreet efficiendi id his, ea illum nonumes luptatum pro. Usu atqui laudem an, insolens gubergren similique est cu. Et vel modus congue vituperata.
+            `,
+            btn: '../../img/main-block/play.svg',
+            backgroungImage: `../../img/main-block/background.png`,
+            decor: {
+                1: `../../img/main-block/decor/01.svg`,
+                2: `../../img/main-block/decor/02.svg`,
+                3: `../../img/main-block/decor/03.svg`,
+                4: `../../img/main-block/decor/04.svg`,
+                5: `../../img/main-block/decor/05.svg`,
+                6: `../../img/main-block/decor/06.svg`,
+                7: `../../img/main-block/decor/07.svg`,
+                8: `../../img/main-block/decor/08.svg`,
+                9: `../../img/main-block/decor/09.svg`,
+                10: `../../img/main-block/decor/10.svg`,
+                11: `../../img/main-block/decor/11.svg`,
+                12: `../../img/main-block/decor/12.svg`,
+                13: `../../img/main-block/decor/13.svg`
+            }
+        }
+    ],
+    blogSwiper: [
+        {
+            id: 1,
+            title: 'How to start planning',
+            text: `
+                    Quidam vocibus eum ne, erat consectetuer
+                    voluptatibus ut nam. Eu usu vidit
+                    tractatos, vero tractatos ius an, in mel
+                    diceret persecuti. Natum petentium
+                    principes mei ea. Tota everti periculis
+                    vis ei, quas tibique pro at, eos ut
+                    decore ...
+            `,
+            decor: '../../img/blog/loupe.svg',
+            img: [
+                `../../img/blog/desk_01.jpg`,
+                `../../img/blog/picnic_02.jpg`,
+                `../../img/blog/call-board_03.jpg`
+            ]
+        },
+        {
+            id: 2,
+            title: 'How to start planning',
+            text: `
+                Quidam vocibus eum ne, erat consectetuer
+                voluptatibus ut nam. Eu usu vidit
+                tractatos, vero tractatos ius an, in mel
+                diceret persecuti.
+                Quidam vocibus eum ne, erat consectetuer
+                voluptatibus ut nam. Eu usu vidit
+                tractatos, vero tractatos ius an...
+            `,
+            decor: '../../img/blog/loupe.svg',
+            img: [
+                `../../img/blog/desk_01.jpg`,
+                `../../img/blog/picnic_02.jpg`,
+                `../../img/blog/call-board_03.jpg`
+            ]
+        },
+        {
+            id: 3,
+            title: 'How to start planning',
+            text: `
+                Quidam vocibus eum ne, erat consectetuer
+                voluptatibus ut nam. Eu usu vidit
+                tractatos, vero tractatos ius an, in mel
+                diceret persecuti. Natum petentium
+                principes mei ea. Tota everti periculis
+                vis ei, quas tibique pro at, eos ut
+                decore ...
+            `,
+            decor: '../../img/blog/loupe.svg',
+            img: [
+                `../../img/blog/desk_01.jpg`,
+                `../../img/blog/picnic_02.jpg`,
+                `../../img/blog/call-board_03.jpg`
+            ]
+        }
+    ],
+    testimonialsSwiper: [
+        {
+            id: 1,
+            text: `
+                Quidam vocibus eum ne, erat consectetuer
+                voluptatibus ut nam. Eu usu vidit tractatos,
+                vero tractatos ius an, in mel diceret persecuti.
+            `,
+            img: `../../img/testimonials/bill.jpg`
+        },
+        {
+            id: 2,
+            text: `
+                Quidam vocibus eum ne, erat consectetuer
+                voluptatibus ut nam. Eu usu vidit tractatos.
+                Quidam vocibus eum ne, erat consectetuer
+                voluptatibus ut nam. Eu usu vidit tractatos.
+            `,
+            img: `../../img/testimonials/bill.jpg`
+        },
+        {
+            id: 3,
+            text: `
+                Quidam vocibus eum ne, erat consectetuer.
+            `,
+            img: `../../img/testimonials/bill.jpg`
+        },
+        {
+            id: 4,
+            text: `
+                Quidam vocibus eum ne, erat consectetuer
+                voluptatibus ut nam. Eu usu vidit tractatos,
+                vero tractatos ius an, in mel diceret persecuti.
+            `,
+            img: `../../img/testimonials/bill.jpg`
+        },
+        {
+            id: 5,
+            text: `
+                Quidam vocibus eum ne, erat consectetuer
+                voluptatibus ut nam. Eu usu vidit tractatos,
+                vero tractatos ius an, in mel diceret persecuti.
+            `,
+            img: `../../img/testimonials/bill.jpg`
+        },
+        {
+            id: 6,
+            text: `
+                Quidam vocibus eum ne, erat consectetuer
+                voluptatibus ut nam. Eu usu vidit tractatos,
+                vero tractatos ius an, in mel diceret persecuti.
+            `,
+            img: `../../img/testimonials/bill.jpg`
+        },
+        {
+            id: 7,
+            text: `
+                Quidam vocibus eum ne, erat consectetuer
+                voluptatibus ut nam. Eu usu vidit tractatos,
+                vero tractatos ius an, in mel diceret persecuti.
+            `,
+            img: `../../img/testimonials/bill.jpg`
+        },
+        {
+            id: 8,
+            text: `
+                Quidam vocibus eum ne, erat consectetuer
+                voluptatibus ut nam. Eu usu vidit tractatos,
+                vero tractatos ius an, in mel diceret persecuti.
+            `,
+            img: `../../img/testimonials/bill.jpg`
+        },
+        {
+            id: 9,
+            text: `
+                Quidam vocibus eum ne, erat consectetuer
+                voluptatibus ut nam. Eu usu vidit tractatos,
+                vero tractatos ius an, in mel diceret persecuti.
+            `,
+            img: `../../img/testimonials/bill.jpg`
+        }
+    ],
+    helpQuestions: [
+        {
+            id: 1,
+            title: 'Reque insolens in vel?',
+            text: `I wish I found some better sounds no one's ever heard
+            I wish I had a better voice that sang some better words
+            I wish I found some chords in an order that is new`,
+            btnUrl: '',
+            btnText: 'Go to documentation'
+        },
+        {
+            id: 2,
+            title: 'Vis rebum error graecis ea, id sit postea accusamus?',
+            text: `I wish I didn't have to rhyme every time I sang
+            I was told when I get older, all my fears would shrink
+            But now I'm insecure, and I care what people think`,
+            btnText: 'Say hello'
+        },
+        {
+            id: 3,
+            title: 'Lorem repudiandae ne nec?',
+            text: `My name's Blurryface and I care what you think
+            My name's Blurryface and I care what you think`,
+            btnUrl: '',
+            btnText: 'Netflix'
+        },
+        {
+            id: 4,
+            title: 'Ad dicit numquam vel. Et eos iudico feugait percipitur?',
+            text: `Wish we could turn back time
+            To the good old days
+            When our momma sang us to sleep
+            But now we're stressed out (oh)`,
+            btnUrl: '',
+            btnText: 'YouTube'
+        },
+        {
+            id: 5,
+            title: 'Sea no dico percipitur. Fierent constituam definitiones id eum?',
+            text: `Wish we could turn back time (oh)
+            To the good old days (oh)
+            When our momma sang us to sleep
+            But now we're stressed out`,
+            btnUrl: '',
+            btnText: 'Google Translate'
+        }
+    ]
+}
+export default data
--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,283 +1,13 @@
 import { isMobile } from './functions.js'
 import { mtrModules } from './modules.js'
+import data from './data.js'
 
-const data = {
-    mainBlockSwiperData: [
-        {
-            id: 1,
-            info: 'PLAN YOUR LIFE',
-            title: `Increase your <span>productivity</span>`,
-            text: `
-                    Brute laoreet efficiendi id his, ea illum nonumes luptatum pro. Usu atqui laudem an, insolens gubergren similique est cu. Et vel modus congue vituperata.
-                `,
-            btn: '../../img/main-block/play.svg',
-            backgroungImage: `../../img/main-block/background.png`,
-            decor: {
-                1: `../../img/main-block/decor/01.svg`,
-                2: `../../img/main-block/decor/02.svg`,
-                3: `../../img/main-block/decor/03.svg`,
-                4: `../../img/main-block/decor/04.svg`,
-                5: `../../img/main-block/decor/05.svg`,
-                6: `../../img/main-block/decor/06.svg`,
-                7: `../../img/main-block/decor/07.svg`,
-                8: `../../img/main-block/decor/08.svg`,
-                9: `../../img/main-block/decor/09.svg`,
-                10: `../../img/main-block/decor/10.svg`,
-                11: `../../img/main-block/decor/11.svg`,
-                12: `../../img/main-block/decor/12.svg`,
-                13: `../../img/main-block/decor/13.svg`
-            }
-        },
-        {
-            id: 2,
-            info: 'Choose yourself',
-            title: `Choose your <span>character</span>`,
-            text: `
-                    I-I-I-I'm from Hell
-                    Deep within the darkest place you'll ever be
-                    I'm from Hell
-                    Do the shit that demons do and make you bleed
-                    I'm from Hell
-                    Make you witness shit that you won't wanna see
-                    What the hell?
-                `,
-            btn: '../../img/main-block/play.svg',
-            backgroungImage: `../../img/main-block/background.png`,
-            decor: {
-                1: `../../img/main-block/decor/01.svg`,
-                2: `../../img/main-block/decor/02.svg`,
-                3: `../../img/main-block/decor/03.svg`,
-                4: `../../img/main-block/decor/04.svg`,
-                5: `../../img/main-block/decor/05.svg`,
-                6: `../../img/main-block/decor/06.svg`,
-                7: `../../img/main-block/decor/07.svg`,
-                8: `../../img/main-block/decor/08.svg`,
-                9: `../../img/main-block/decor/09.svg`,
-                10: `../../img/main-block/decor/10.svg`,
-                11: `../../img/main-block/decor/11.svg`,
-                12: `../../img/main-block/decor/12.svg`,
-                13: `../../img/main-block/decor/13.svg`
-            }
-        },
-        {
-            id: 3,
-            info: 'Just feel it',
-            title: `Increase your <span>productivity</span>`,
-            text: `
-                    Brute laoreet efficiendi id his, ea illum nonumes luptatum pro. Usu atqui laudem an, insolens gubergren similique est cu. Et vel modus congue vituperata.
-            `,
-            btn: '../../img/main-block/play.svg',
-            backgroungImage: `../../img/main-block/background.png`,
-            decor: {
-                1: `../../img/main-block/decor/01.svg`,
-                2: `../../img/main-block/decor/02.svg`,
-                3: `../../img/main-block/decor/03.svg`,
-                4: `../../img/main-block/decor/04.svg`,
-                5: `../../img/main-block/decor/05.svg`,
-                6: `../../img/main-block/decor/06.svg`,
-                7: `../../img/main-block/decor/07.svg`,
-                8: `../../img/main-block/decor/08.svg`,
-                9: `../../img/main-block/decor/09.svg`,
-                10: `../../img/main-block/decor/10.svg`,
-                11: `../../img/main-block/decor/11.svg`,
-                12: `../../img/main-block/decor/12.svg`,
-                13: `../../img/main-block/decor/13.svg`
-            }
-        }
-    ],
-    blogSwiper: [
-        {
-            id: 1,
-            title: 'How to start planning',
-            text: `
-                    Quidam vocibus eum ne, erat consectetuer
-                    voluptatibus ut nam. Eu usu vidit
-                    tractatos, vero tractatos ius an, in mel
-                    diceret persecuti. Natum petentium
-                    principes mei ea. Tota everti periculis
-                    vis ei, quas tibique pro at, eos ut
-                    decore ...
-            `,
-            decor: '../../img/blog/loupe.svg',
-            img: [
-                `../../img/blog/desk_01.jpg`,
-                `../../img/blog/picnic_02.jpg`,
-                `../../img/blog/call-board_03.jpg`
-            ]
-        },
-        {
-            id: 2,
-            title: 'How to start planning',
-            text: `
-                Quidam vocibus eum ne, erat consectetuer
-                voluptatibus ut nam. Eu usu vidit
-                tractatos, vero tractatos ius an, in mel
-                diceret persecuti.
-                Quidam vocibus eum ne, erat consectetuer
-                voluptatibus ut nam. Eu usu vidit
-                tractatos, vero tractatos ius an...
-            `,
-            decor: '../../img/blog/loupe.svg',
-            img: [
-                `../../img/blog/desk_01.jpg`,
-                `../../img/blog/picnic_02.jpg`,
-                `../../img/blog/call-board_03.jpg`
-            ]
-        },
-        {
-            id: 3,
-            title: 'How to start planning',
-            text: `
-                Quidam vocibus eum ne, erat consectetuer
-                voluptatibus ut nam. Eu usu vidit
-                tractatos, vero tractatos ius an, in mel
-                diceret persecuti. Natum petentium
-                principes mei ea. Tota everti periculis
-                vis ei, quas tibique pro at, eos ut
-                decore ...
-            `,
-            decor: '../../img/blog/loupe.svg',
-            img: [
-                `../../img/blog/desk_01.jpg`,
-                `../../img/blog/picnic_02.jpg`,
-                `../../img/blog/call-board_03.jpg`
-            ]
-        }
-    ],
-    testimonialsSwiper: [
-        {
-            id: 1,
-            text: `
-                Quidam vocibus eum ne, erat consectetuer
-                voluptatibus ut nam. Eu usu vidit tractatos,
-                vero tractatos ius an, in mel diceret persecuti.
-            `,
-            img: `../../img/testimonials/bill.jpg`
-        },
-        {
-            id: 2,
-            text: `
-                Quidam vocibus eum ne, erat consectetuer
-                voluptatibus ut nam. Eu usu vidit tractatos.
-                Quidam vocibus eum ne, erat consectetuer
-                voluptatibus ut nam. Eu usu vidit tractatos.
-            `,
-            img: `../../img/testimonials/bill.jpg`
-        },
-        {
-            id: 3,
-            text: `
-                Quidam vocibus eum ne, erat consectetuer.
-            `,
-            img: `../../img/testimonials/bill.jpg`
-        },
-        {
-            id: 4,
-            text: `
-                Quidam vocibus eum ne, erat consectetuer
-                voluptatibus ut nam. Eu usu vidit tractatos,
-                vero tractatos ius an, in mel diceret persecuti.
-            `,
-            img: `../../img/testimonials/bill.jpg`
-        },
-        {
-            id: 5,
-            text: `
-                Quidam vocibus eum ne, erat consectetuer
-                voluptatibus ut nam. Eu usu vidit tractatos,
-                vero tractatos ius an, in mel diceret persecuti.
-            `,
-            img: `../../img/testimonials/bill.jpg`
-        },
-        {
-            id: 6,
-            text: `
-                Quidam vocibus eum ne, erat consectetuer
-                voluptatibus ut nam. Eu usu vidit tractatos,
-                vero tractatos ius an, in mel diceret persecuti.
-            `,
-            img: `../../img/testimonials/bill.jpg`
-        },
-        {
-            id: 7,
-            text: `
-                Quidam vocibus eum ne, erat consectetuer
-                voluptatibus ut nam. Eu usu vidit tractatos,
-                vero tractatos ius an, in mel diceret persecuti.
-            `,
-            img: `../../img/testimonials/bill.jpg`
-        },
-        {
-            id: 8,
-            text: `
-                Quidam vocibus eum ne, erat consectetuer
-                voluptatibus ut nam. Eu usu vidit tractatos,
-                vero tractatos ius an, in mel diceret persecuti.
-            `,
-            img: `../../img/testimonials/bill.jpg`
-        },
-        {
-            id: 9,
-            text: `
-                Quidam vocibus eum ne, erat consectetuer
-                voluptatibus ut nam. Eu usu vidit tractatos,
-                vero tractatos ius an, in mel diceret persecuti.
-            `,
-            img: `../../img/testimonials/bill.jpg`
-        }
-    ],
-    helpContent: [
-        {
-            id: 1,
-            title: 'Reque insolens in vel?',
-            desc: `I wish I found some better sounds no one's ever heard
-            I wish I had a better voice that sang some better words
-            I wish I found some chords in an order that is new`,
-            btnText: 'Go to documentation'
-        },
-        {
-            id: 2,
-            title: 'Vis rebum error graecis ea, id sit postea accusamus?',
-            desc: `I wish I didn't have to rhyme every time I sang
-            I was told when I get older, all my fears would shrink
-            But now I'm insecure, and I care what people think`,
-            btnText: 'Say hello'
-        },
-        {
-            id: 3,
-            title: 'Lorem repudiandae ne nec?',
-            desc: `My name's Blurryface and I care what you think
-            My name's Blurryface and I care what you think`,
-            btnText: 'Netflix'
-        },
-        {
-            id: 4,
-            title: 'Ad dicit numquam vel. Et eos iudico feugait percipitur?',
-            desc: `Wish we could turn back time
-            To the good old days
-            When our momma sang us to sleep
-            But now we're stressed out (oh)`,
-            btnText: 'YouTube'
-        },
-        {
-            id: 5,
-            title: 'Sea no dico percipitur. Fierent constituam definitiones id eum?',
-            desc: `Wish we could turn back time (oh)
-            To the good old days (oh)
-            When our momma sang us to sleep
-            But now we're stressed out`,
-            btnText: 'Google Translate'
-        }
-    ]
-}
-
-// const items = [...document.querySelectorAll('.number')]
 
 const percents = document.querySelector('.item-customers__percents')
 const number = document.querySelector('.item-users__number')
 
+let isResizeble = false
 window.addEventListener('scroll', () => {
-    let isResizeble = false
     const scrollHeight = Math.floor(window.pageYOffset)
 
     if (!isResizeble && scrollHeight > 1350) {
@@ -643,3 +373,42 @@ function displayTestimonialsSwiper(data) {
 
 displayTestimonialsSwiper(data)
 //! </Testimonials Swiper>
+
+//! <Help Questions>
+function displayBlockHelp(data) {
+    const helpWrapper = document.querySelector('.spollers-help')
+    const { helpQuestions } = data
+
+    let displayBlockHelp = helpQuestions.map((item) => {
+        const { id, title, text, btnUrl, btnText } = item
+
+        return `
+        <div class="spollers-help__item">
+            <button
+                type="button"
+                data-spoller
+                class="spollers-help__title"
+            >
+                ${title}
+            </button>
+            <div class="spollers-help__body body-spollers">
+                <div class="body-spollers__body">
+                    <div class="body-spollers__text text">
+                        ${text}
+                    </div>
+                    <a
+                        class="body-spollers__link btn"
+                        href="${btnUrl}"
+                        >${btnText}</a
+                    >
+                </div>
+            </div>
+        </div>
+        `
+    })
+    displayBlockHelp = displayBlockHelp.join('')
+    helpWrapper.innerHTML = displayBlockHelp
+}
+
+displayBlockHelp(data)
+//! </Help Questions>
